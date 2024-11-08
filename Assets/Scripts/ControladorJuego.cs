6638e23d912b53b3268b236f0141437885c4d362
Johann <fumeroperezjohann@gmail.com>
2024-11-08 09:45:40 +0000

    Objetos y Acciones
    Cree todos los objetos en el Script y también las acciones que van a gestionar cada uno de ellos

--- Assets/Scripts/ControladorJuego.cs
+++ Assets/Scripts/ControladorJuego.cs
@@ -1,18 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ControladorJuego : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    GameObject BotonInicio;
+    [SerializeField]
+    GameObject EditorEscena;
+    [SerializeField]
+    GameObject MenuObjetos;
+    [SerializeField]
+    GameObject MenuMR, MenuP, MenuMV, MenuM;
+    [SerializeField]
+    GameObject Contruccion, Mover, Rotar, Eliminar;
+    [SerializeField]
+    GameObject AirHock, ArcadeMachine, Basketball, DanceMachine, Gambling, Pintaball;
+    [SerializeField]
+    GameObject Chica, Chico;
+    [SerializeField]
+    GameObject TicketMachine, VendingMachine, ClawMachine;
+    [SerializeField]
+    GameObject CashRegister, Prizes, PrizeWheel, Column;
+    
+
     void Start()
     {
-        
+        BotonInicio.SetActive(true);
+        EditorEscena.SetActive(false);
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        Play();
+    }
+
+    private void Play()
+    {
+        if (BotonInicio == false)
+        {
+            EditorEscena.SetActive(true);
+            
+        }
+    }
+    private void Construcion()
+    {
+        if(Contruccion == false)
+        {
+            MenuObjetos.SetActive(true);
+        }
+    }
+    private void MaquinaRecreativa()
+    {
+
+    }
+    private void Persona() 
+    {
+
+    }
+    private void Aleatorio()
+    {
+
+    }
+    private void MaquinasVendedoras()
+    {
+
+    }
+    private void Muebles()
+    {
+
+    }
+    private void Movimiento()
+    {
+
+    }
+    private void Rotacion()
+    {
+
+    }
+    private void borrar()
+    {
+
     }
 }
